cf829a9d0acbd47e812b5b6e302b61cdce56040d
yjson <handyejee14@gmail.com>
2024-10-02 23:13:17 +0900

    initial commit

---
+++ src/test/java/helloworld/studytogether/StudyTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package helloworld.studytogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTogetherApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
