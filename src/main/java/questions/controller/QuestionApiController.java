7db4372b03608263f9cd9ca34fadb938451a7745
yjson <handyejee14@gmail.com>
2024-10-14 09:51:14 +0900

    feat : 질문 등록 controller 메서드 정의

---
+++ src/main/java/questions/controller/QuestionApiController.java
@@ -0,0 +1,33 @@
+package questions.controller;
+
+import jakarta.validation.Valid;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import questions.dto.AddQuestionRequestDto;
+import questions.dto.AddQuestionResponseDto;
+import questions.entity.Question;
+import questions.service.QuestionService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/question")
+public class QuestionApiController {
+
+  private final QuestionService questionService;
+
+  @PostMapping
+  public ResponseEntity<AddQuestionResponseDto> addQuestion(@RequestBody @Valid AddQuestionRequestDto addQuestionRequest)
+      throws IOException {
+    Question savedQuestion = questionService.saveQuestion(addQuestionRequest);
+    AddQuestionResponseDto responseDto = AddQuestionResponseDto.fromEntity(savedQuestion);
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(responseDto);
+  }
+}
