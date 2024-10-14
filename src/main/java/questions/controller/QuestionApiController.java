7361dc75d1e5e1fbfe212690ebcf3710c0132492
yjson <handyejee14@gmail.com>
2024-10-14 16:56:17 +0900

    fix : 질문 등록시 이미지도 받아올 수 있도록 ModelAttribute 어노테이션으로 변경

--- src/main/java/questions/controller/QuestionApiController.java
+++ src/main/java/questions/controller/QuestionApiController.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import questions.dto.AddQuestionRequestDto;
@@ -22,7 +22,7 @@ public class QuestionApiController {
   private final QuestionService questionService;
 
   @PostMapping
-  public ResponseEntity<AddQuestionResponseDto> addQuestion(@RequestBody @Valid AddQuestionRequestDto addQuestionRequest)
+  public ResponseEntity<AddQuestionResponseDto> addQuestion(@ModelAttribute @Valid AddQuestionRequestDto addQuestionRequest)
       throws IOException {
     Question savedQuestion = questionService.saveQuestion(addQuestionRequest);
     AddQuestionResponseDto responseDto = AddQuestionResponseDto.fromEntity(savedQuestion);
