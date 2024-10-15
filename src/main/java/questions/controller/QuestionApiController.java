b2b2aedc760613967ecf5ab07e4500e6bd0e0d04
yjson <handyejee14@gmail.com>
2024-10-15 11:02:53 +0900

    fix : 권한 부여를 위한 preauthorize 어노테이션 추가

--- src/main/java/questions/controller/QuestionApiController.java
+++ src/main/java/questions/controller/QuestionApiController.java
@@ -3,8 +3,10 @@ package questions.controller;
 import jakarta.validation.Valid;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import questions.dto.AddQuestionResponseDto;
 import questions.entity.Question;
 import questions.service.QuestionService;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/question")
@@ -22,6 +25,7 @@ public class QuestionApiController {
   private final QuestionService questionService;
 
   @PostMapping
+  @PreAuthorize("hasAuthority('USER')")
   public ResponseEntity<AddQuestionResponseDto> addQuestion(@ModelAttribute @Valid AddQuestionRequestDto addQuestionRequest)
       throws IOException {
     Question savedQuestion = questionService.saveQuestion(addQuestionRequest);
