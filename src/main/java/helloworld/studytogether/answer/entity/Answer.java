47899c3dfcc0e8756f27ad26233fb811c3c1efb6
JIN <akasagi@naver.com>
2024-10-16 00:22:09 +0900

    BaseEntity 추가

--- src/main/java/helloworld/studytogether/answer/entity/Answer.java
+++ src/main/java/helloworld/studytogether/answer/entity/Answer.java
@@ -2,36 +2,33 @@ package helloworld.studytogether.answer.entity;
 
 import helloworld.studytogether.user.entity.User;
 import questions.entity.Question;
+import common.entity.BaseEntity;
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "answers")
-public class Answer {
+public class Answer extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "answer_id")
     private Long id;
 
-     @ManyToOne(fetch = FetchType.LAZY)
-     @JoinColumn(name = "question_id", nullable = false)
-     private Question question;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
 
-     @ManyToOne(fetch = FetchType.LAZY)
-     @JoinColumn(name = "parent_answer_id")
-     private Answer parentAnswer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_answer_id")
+    private Answer parentAnswer;
 
-     @ManyToOne(fetch = FetchType.LAZY)
-     @JoinColumn(name = "userId", nullable = false)
-     private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
@@ -44,12 +41,4 @@ public class Answer {
 
     @Column(name = "is_selected", nullable = false)
     private boolean isSelected = false;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
 }
