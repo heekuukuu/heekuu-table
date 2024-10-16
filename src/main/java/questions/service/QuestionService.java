e0bb5c164112fe008973fb8c65b70db762d09004
JIN <akasagi@naver.com>
2024-10-17 01:09:07 +0900

    삭제

--- src/main/java/questions/service/QuestionService.java
+++
@@ -1,9 +0,0 @@
-package questions.service;
-
-import java.io.IOException;
-import questions.dto.AddQuestionRequestDto;
-import questions.entity.Question;
-
-public interface QuestionService {
-  Question saveQuestion(AddQuestionRequestDto requestDto) throws IOException;
-}
