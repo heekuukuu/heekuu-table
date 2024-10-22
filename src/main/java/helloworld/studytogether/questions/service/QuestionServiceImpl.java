7979f66ec3a5e954a3e360fbd427c7c6e5dc1484
yjson <handyejee14@gmail.com>
2024-10-23 00:22:27 +0900

    feat: 사용자별 질문조회 서비스 로직 구현

--- src/main/java/helloworld/studytogether/questions/service/QuestionServiceImpl.java
+++ src/main/java/helloworld/studytogether/questions/service/QuestionServiceImpl.java
@@ -1,14 +1,16 @@
 package helloworld.studytogether.questions.service;
 
+import helloworld.studytogether.questions.dto.GetQuestionResponseDto;
 import helloworld.studytogether.user.entity.User;
 import helloworld.studytogether.user.repository.UserRepository;
 import java.io.IOException;
-import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import helloworld.studytogether.questions.dto.AddQuestionRequestDto;
+import helloworld.studytogether.questions.dto.QuestionRequestDto;
 import helloworld.studytogether.questions.entity.Question;
 import helloworld.studytogether.questions.repository.QuestionRepository;
 
@@ -24,18 +26,16 @@ public class QuestionServiceImpl implements QuestionService {
    * 새로운 질문을 저장합니다.
    *
    * @param request 저장할 질문 항목의 DTO.
-   * @param userId  유저의 ID (서버에서 전달받음)
    * @return 저장한 질문 정보를 반환합니다.
    * @throws IOException 이미지 처리 중 문제가 발생할 경우 발생합니다.
    */
   @Transactional
-  @Override
-  public Question saveQuestion(AddQuestionRequestDto request, Long userId) throws IOException {
-    // userId로 사용자 조회
-    User user = userRepository.findById(userId)
+  public Question saveQuestion(QuestionRequestDto request) throws IOException {
+
+    User user = userRepository.findByUserId(request.getUserId())
         .orElseThrow(() -> {
-          log.error("User not found with ID: {}", userId);
-          return new IllegalArgumentException("사용자를 찾을 수 없습니다: " + userId);
+          log.error("User not found with ID: {}", request.getUserId());
+          return new IllegalArgumentException("사용자를 찾을 수 없습니다: " + request.getUserId());
         });
 
     byte[] imageBytes = null;
@@ -50,8 +50,7 @@ public class QuestionServiceImpl implements QuestionService {
       }
     }
 
-    // 새로운 질문 생성
-    Question question = toEntity(request, user, imageBytes);
+    Question question = request.toEntity(request, user, imageBytes);
 
     try {
       Question savedQuestion = questionRepository.save(question);
@@ -64,16 +63,17 @@ public class QuestionServiceImpl implements QuestionService {
     }
   }
 
-  Date now = new Date();
-
-  private Question toEntity(AddQuestionRequestDto request, User user, byte[] imageBytes) {
-    return Question.builder()
-        .user(user)
-        .title(request.getTitle())
-        .subjectName(request.getSubjectName())
-        .content(request.getContent())
-        .createdAt(now)
-        .image(imageBytes)
-        .build();
+  /**
+   * 사용자별 등록한 질문 목록을 조회합니다.
+   * @param userId 조회할 질문 목록의 사용자 ID
+   * @param pageable 페이지네이션 정보
+   * @return 조회한 질문 목록을 반환합니다.
+   */
+  public Page<GetQuestionResponseDto> getQuestionList(Long userId, Pageable pageable) {
+    Page<Question> questions = questionRepository.findAllByUser_UserId(userId, pageable);
+    return questions.map(GetQuestionResponseDto::fromEntity);
   }
+
+
+
 }
