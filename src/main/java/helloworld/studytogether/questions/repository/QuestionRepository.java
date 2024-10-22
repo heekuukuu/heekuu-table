0daedfa0fa23aa7ca9efe3f49bb280a8cf9317f1
yjson <handyejee14@gmail.com>
2024-10-22 15:34:25 +0900

    feat: 사용자별 질문 조회 메서드 구현

--- src/main/java/helloworld/studytogether/questions/repository/QuestionRepository.java
+++ src/main/java/helloworld/studytogether/questions/repository/QuestionRepository.java
@@ -1,8 +1,11 @@
 package helloworld.studytogether.questions.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import helloworld.studytogether.questions.entity.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
+  Page<Question> findAllByUser_UserId(Long userId, Pageable pageable);
 }
