a7a6887157db9f158abc9af7aed8957a322d5bf7
yjson <handyejee14@gmail.com>
2024-10-23 16:43:30 +0900

    feat : 조회 api 호출 시 인증된 사용자 메서드로 조회하도록 변경

--- src/main/java/helloworld/studytogether/questions/controller/QuestionApiController.java
+++ src/main/java/helloworld/studytogether/questions/controller/QuestionApiController.java
@@ -1,7 +1,8 @@
 package helloworld.studytogether.questions.controller;
 
+import helloworld.studytogether.jwt.util.SecurityUtil;
+import helloworld.studytogether.questions.dto.QuestionRequest;
 import helloworld.studytogether.questions.dto.GetQuestionResponseDto;
-import helloworld.studytogether.questions.dto.QuestionRequestDto;
 import helloworld.studytogether.questions.dto.AddQuestionResponseDto;
 import helloworld.studytogether.questions.entity.Question;
 import helloworld.studytogether.user.dto.CustomUserDetails;
@@ -20,24 +21,23 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import helloworld.studytogether.questions.service.QuestionService;
-import helloworld.studytogether.user.dto.CustomUserDetails;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/questions")
+@RequestMapping("/question")
 public class QuestionApiController {
 
+  private final SecurityUtil securityUtil;
   private final QuestionService questionService;
 
   @PostMapping
   @PreAuthorize("hasAuthority('USER')")
   public ResponseEntity<AddQuestionResponseDto> addQuestion(
-      @ModelAttribute @Valid QuestionRequestDto addQuestionRequest, Authentication authentication)
+      @ModelAttribute @Valid QuestionRequest addQuestionRequest, Authentication authentication)
       throws IOException {
 
     // 인증된 유저의 정보를 가져와 userId 추출
@@ -52,17 +52,22 @@ public class QuestionApiController {
         .body(responseDto);
   }
 
-  // 사용자별 등록한 문제조회
+  /**
+   * 사용자 별 등록한 전체 문제를 조회합니다.
+   * @param pageable 페이징 정보
+   *                 - page : 페이지 번호
+   *                 - size : 페이지당 항목 수
+   *                 - sort : 정렬 기준 (기본값: createdAt, DESC)
+   * @param authentication 현재 인증된 사용자 정보
+   * @return 페이징된 문제목록 반환
+   */
   @GetMapping("/my-questions")
   public ResponseEntity<Page<GetQuestionResponseDto>> getUserQuestions(
       @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC)
       Pageable pageable, Authentication authentication) {
 
-    CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-    Long userId = customUserDetails.getUserId();
-
-    Page<GetQuestionResponseDto> questions =
-        questionService.getQuestionList(userId, pageable);
+    Long userId = securityUtil.getCurrentUserId(authentication);
+    Page<GetQuestionResponseDto> questions = questionService.getQuestionList(userId, pageable);
     return ResponseEntity.ok(questions);
   }
 
