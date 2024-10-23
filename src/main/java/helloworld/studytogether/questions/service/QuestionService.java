0f02acd348dd35c66a17a2daf0858772a236c82e
yjson <handyejee14@gmail.com>
2024-10-23 15:57:03 +0900

    fix : request dto 변환 메서드 이동

--- src/main/java/helloworld/studytogether/questions/service/QuestionService.java
+++ src/main/java/helloworld/studytogether/questions/service/QuestionService.java
@@ -1,13 +1,13 @@
 package helloworld.studytogether.questions.service;
 
 import helloworld.studytogether.questions.dto.GetQuestionResponseDto;
-import helloworld.studytogether.questions.dto.QuestionRequestDto;
+import helloworld.studytogether.questions.dto.QuestionRequest;
 import helloworld.studytogether.questions.entity.Question;
 import java.io.IOException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface QuestionService {
-  Question saveQuestion(QuestionRequestDto requestDto) throws IOException;
+  Question saveQuestion(QuestionRequest requestDto, Long userId) throws IOException;
   Page<GetQuestionResponseDto> getQuestionList(Long userId, Pageable pageable);
 }
