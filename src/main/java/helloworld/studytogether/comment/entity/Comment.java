1877a0195d65d56ebf030524f9291a4f4965fc6b
JIN <akasagi@naver.com>
2024-10-25 04:14:14 +0900

    대댓글 엔티티 작성

---
+++ src/main/java/helloworld/studytogether/comment/entity/Comment.java
@@ -0,0 +1,45 @@
+package helloworld.studytogether.comment.entity;
+
+import helloworld.studytogether.answer.entity.Answer;
+import helloworld.studytogether.user.entity.User;
+import helloworld.studytogether.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id", nullable = false)
+    private Answer answerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // 생성자, getter, setter 추가
+}
