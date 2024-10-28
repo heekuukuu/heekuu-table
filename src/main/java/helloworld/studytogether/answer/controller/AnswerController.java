313d452cb8393efddafced8ff9c5e6fad1ce1335
yjson <handyejee14@gmail.com>
2024-10-28 19:04:40 +0900

    chore : 들여쓰기

--- src/main/java/helloworld/studytogether/answer/controller/AnswerController.java
+++ src/main/java/helloworld/studytogether/answer/controller/AnswerController.java
@@ -48,23 +48,23 @@ public class AnswerController {
     return ResponseEntity.noContent().build();
   }
 
-   // 답변 좋아요
-   @PostMapping("/{answerId}/like")
-   public ResponseEntity<Void> likeAnswer(@PathVariable("answerId") Long id) {
-       answerService.likeAnswer(id);
-       return ResponseEntity.ok().build();
-   }
+  // 답변 좋아요
+  @PostMapping("/{answerId}/like")
+  public ResponseEntity<Void> likeAnswer(@PathVariable("answerId") Long id) {
+    answerService.likeAnswer(id);
+    return ResponseEntity.ok().build();
+  }
 
-   // 답변 좋아요 취소
-   @DeleteMapping("/{answerId}/like")
-   public ResponseEntity<Void> unlikeAnswer(@PathVariable("answerId") Long id) {
-       answerService.unlikeAnswer(id);
-       return ResponseEntity.ok().build();
-   }
+  // 답변 좋아요 취소
+  @DeleteMapping("/{answerId}/like")
+  public ResponseEntity<Void> unlikeAnswer(@PathVariable("answerId") Long id) {
+    answerService.unlikeAnswer(id);
+    return ResponseEntity.ok().build();
+  }
 
-   // 답변 채택
-   @PatchMapping("/questions/{questionId}/select-answer/{answerId}")
-   public ResponseEntity<Void> selectAnswer(@PathVariable("questionId") Long questionId, @PathVariable("answerId") Long answerId) {
-       answerService.selectAnswer(questionId, answerId);
-       return ResponseEntity.ok().build();
-   }
+  // 답변 채택
+  @PatchMapping("/questions/{questionId}/select-answer/{answerId}")
+  public ResponseEntity<Void> selectAnswer(@PathVariable("questionId") Long questionId, @PathVariable("answerId") Long answerId) {
+    answerService.selectAnswer(questionId, answerId);
+    return ResponseEntity.ok().build();
+  }
