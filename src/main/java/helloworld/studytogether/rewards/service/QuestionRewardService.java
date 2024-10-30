7bc1a16322d48b0d6635b8f775edab9f202536d9
heekuukuu <gmlrud2di@gmail.com>
2024-10-30 13:57:24 +0900

    fix:안쓰는주석삭제

--- src/main/java/helloworld/studytogether/rewards/service/QuestionRewardService.java
+++ src/main/java/helloworld/studytogether/rewards/service/QuestionRewardService.java
@@ -23,7 +23,7 @@ public class QuestionRewardService {
   public void rewardForAcceptedAnswer(Long userId) {
     int rewardPoints = 20; // 답변 채택 보상 포인트
     String actionDescription = "답변 채택";
-    
+
     rewardService.addReward(userId, actionDescription, rewardPoints);
   }
 
